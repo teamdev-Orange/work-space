b5fce50cd44d605efd2327d3d61e3652a777e532
55Kamiryo <r_kami@keio.jp>
2023-10-29 10:25:52 +0900

    gameloopelse

--- game.js
+++ game.js
@@ -83,24 +83,6 @@ const blockColor = {
   "6":"orange",
 }
 
-class Asset {
-  constructor(x=0,y=0){
-    this.type=Math.floor(Math.random() * 7);
-    this.x=x
-    this.y=y
-    //ブロックの移動を判定するときに空のブロックを作る
-    if (this.type>=0) {
-      this.setType(this.type)
-    }
-  }
-
-  setType(BlockType){
-    this.BlockType=BlockType
-    this.BlockColor=Asset.blockColor[BlockType]
-  }
-
-}
-
 class Block {
     constructor(x, y, blockType) { //コンストラクタで初期位置は0に//
         this.x = 0;
@@ -256,8 +238,13 @@ function gameLoop(time) {
         if (currentBlock.isValidMove(0, 1, currentBlock.pattern)) {
             currentBlock.move(0, 1);
         } else {
-            // ブロックがボードの底や他のブロックに接触した場合の処理
-            // 例: 新しいブロックを生成する、行が完全に埋まっているかを確認するなど
+          // ブロックが移動できない場合、固定して新しいブロックを生成
+            fixBlocklToBoard();
+            currentBlock = new Block();
+            if (isGameOver()) {
+              endGame();
+              return;
+            }
         }
         lastDropTime = time;
     }
@@ -268,6 +255,31 @@ function gameLoop(time) {
     updateGame(deltaTime);
 
     requestAnimationFrame(gameLoop);
+}
+
+function GameOver() {
+  // ゲームオーバーの条件をチェックする
+  return gameBoard.board[0].some(cell => cell !== 0);
+}
+
+function endGame() {
+  // ゲームオーバー時の処理
+  isGameRunning = false;
+  showModalWithScore();
+}
+
+function fixBlockToBoard() {
+  // currentBlockの各セルをゲームボードに追加する
+  for (let y = 0; y < currentBlock.pattern.length; y++) {
+      for (let x = 0; x < currentBlock.pattern[y].length; x++) {
+          if (currentBlock.pattern[y][x]) {
+              gameBoard.board[currentBlock.y + y][currentBlock.x + x] = 1;
+          }
+      }
+  }
+
+  // 完全に埋まった行をクリア
+  gameBoard.checkFullRows();
 }
 
   let isGameOver = false;
@@ -322,6 +334,8 @@ class GameBoard {
     for (let y = 0; y < this.board.length; y++) {
       if (this.board[y].every(cell => cell === 1)) {
         this.clearRow(y);
+        this.board.splice(y, 1);
+        this.board.unshift(new Array(10).fill(0));
       }
     }
   }
@@ -330,4 +344,15 @@ class GameBoard {
     this.board.splice(rowNumber, 1);
     this.board.unshift(new Array(10).fill(0));
   }
+}
+
+function draw() {
+  // ゲームボードとブロックの描画
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGameBoard();
+  currentBlock.drawBlockPattern(ctx, currentBlock.type);
+}
+
+function drawGameBoard() {
+  // ゲームボードのセルを描画するロジック
 }
